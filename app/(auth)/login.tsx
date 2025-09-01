1e81ffa5f4056823ed1515b6ab16f84aed542c5a
shmuli rosenberg <shmuli2015@gmail.com>
2025-09-02 00:12:52 +0300

    Enhance login screen layout and styling. Updated the Login component to include a welcoming message and improved visual structure with a new text container. Added styles for title and subtitle to enhance user experience.

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,12 +1,43 @@
 import GradientBackground from "@/components/GradientBackground"
-import { Text } from "react-native"
+import Input from "@/components/Input";
+import { StyleSheet, Text, View, Dimensions } from "react-native"
+
+const { height } = Dimensions.get('window');
 
 const Login = () => {
     return (
         <GradientBackground>
-            <Text>Login</Text>
+            <View style={styles.container}>
+                <View style={styles.textContainer}>
+                    <Text style={styles.title}>Welcome Back</Text>
+                    <Text style={styles.subtitle}>Log in to manage your household expenses.</Text>
+                </View>
+            </View>
         </GradientBackground>
     )
 }
 
 export default Login;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    textContainer: {
+        alignItems: 'center',
+        marginTop: height * 0.25,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: 'light',
+        textAlign: 'center',
+    },
+});
